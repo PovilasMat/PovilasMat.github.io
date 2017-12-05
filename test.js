19958af253c9403c7f22235b8ff0a82f98bab633
PovilasMat <povilas.matusevicius@gmail.com>
2017-12-05 14:23:18 +0100

    up

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ var y = d3.scale.linear()
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");	
 	
 d3.tsv(
