13d7b96f9af5bd0394f67effde97841e524a1f71
PovilasMat <povilas.matusevicius@gmail.com>
2017-12-19 15:07:12 +0100

    a

---
+++ project4/source/handPCA.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python
+
+import numpy as np
+from sklearn.decomposition import PCA
+
+# Load csv file
+hands     = np.loadtxt('hands.csv', delimiter=',')
+
+# Perform PCA
+hands_pca = PCA().fit_transform(hands)
+
+# If there are fewer points than dimensions the PCA will automatically remove some columns. 
+# They're not needed, but for completeness I readd them
+if hands.shape[0]<hands.shape[1]:
+    hands_pca = np.append(hands_pca, np.zeros( (hands.shape[0],hands.shape[1]-hands.shape[0])),axis=1)
+
+header = ""
+for i in range(hands_pca.shape[1]):
+    if i != hands_pca.shape[1]:
+        header = header + "feature" + str(i+1) + ','
+    else:
+        header = header + "feature" + str(i+1)
+# Store PCA
+np.savetxt('hands_pca.csv',hands_pca,fmt="%11.8f", delimiter=',', header=header)
+
